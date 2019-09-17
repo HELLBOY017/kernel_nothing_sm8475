64c9cfc8997ad825f590b7a12aadd536998a401c
Prashant Singh <prasin@codeaurora.org>
2019-09-18 02:26:45 +0530

    msm/sde: prevent double register of input handler
    During Dynamic mode switch case, input_register_handler
    is called even after it is registered leading to
    list double add warnings and panic.
    
    Change-Id: I312112deb08c998af516252f2117bc629eb13b37
    Signed-off-by: Prashant Singh <prasin@codeaurora.org>

--- techpack/display/msm/sde/sde_encoder.c
+++ techpack/display/msm/sde/sde_encoder.c
@@ -731,6 +731,7 @@ void sde_encoder_destroy(struct drm_encoder *drm_enc)
 
 	kfree(sde_enc->input_handler);
 	sde_enc->input_handler = NULL;
+	sde_enc->input_handler_registered = false;
 
 	kfree(sde_enc);
 }
@@ -2831,6 +2832,7 @@ static int _sde_encoder_input_handler(
 	input_handler->id_table = sde_input_ids;
 
 	sde_enc->input_handler = input_handler;
+	sde_enc->input_handler_registered = false;
 
 	return rc;
 }
@@ -3103,7 +3105,19 @@ static void sde_encoder_virt_enable(struct drm_encoder *drm_enc)
 		return;
 	}
 
-	_sde_encoder_input_handler_register(drm_enc);
+	/* register input handler if not already registered */
+	if (sde_enc->input_handler && !sde_enc->input_handler_registered &&
+			!msm_is_mode_seamless_dms(msm_mode) &&
+		sde_encoder_check_curr_mode(drm_enc, MSM_DISPLAY_CMD_MODE) &&
+			!msm_is_mode_seamless_dyn_clk(msm_mode)) {
+		_sde_encoder_input_handler_register(drm_enc);
+		if (!sde_enc->input_handler || !sde_enc->input_handler->private)
+			SDE_ERROR(
+			"input handler registration failed, rc = %d\n", ret);
+		else
+			sde_enc->input_handler_registered = true;
+	}
+
 	c_state = to_sde_connector_state(sde_enc->cur_master->connector->state);
 	if (!c_state) {
 		SDE_ERROR("invalid connector state\n");
@@ -3222,7 +3236,11 @@ static void sde_encoder_virt_disable(struct drm_encoder *drm_enc)
 		sde_encoder_wait_for_event(drm_enc, MSM_ENC_TX_COMPLETE);
 	}
 
-	_sde_encoder_input_handler_unregister(drm_enc);
+	if (sde_enc->input_handler && sde_enc->input_handler_registered &&
+		sde_encoder_check_curr_mode(drm_enc, MSM_DISPLAY_CMD_MODE)) {
+		_sde_encoder_input_handler_unregister(drm_enc);
+		sde_enc->input_handler_registered = false;
+	}
 
 	flush_delayed_work(&sde_conn->status_work);
 	/*
