28b0e6a7234217be9d29ae5a17527a6a772084df
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-19 19:42:30 -0800

    disp: msm: Affine important kthreads to the prime CPU cluster
    These kthreads (particularly crtc_event and crtc_commit) play a major
    role in rendering frames to the display, so affine them to the prime CPU
    cluster, matching the DRM IRQ.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Change-Id: I0ba8fe2c7a3c87f08cd1666b4a7533ca5d101e4c

--- techpack/display/msm/msm_drv.c
+++ techpack/display/msm/msm_drv.c
@@ -607,7 +607,8 @@ static int msm_drm_display_thread_create(struct msm_drm_private *priv, struct dr
 		kthread_init_worker(&priv->disp_thread[i].worker);
 		priv->disp_thread[i].dev = ddev;
 		priv->disp_thread[i].thread =
-			kthread_run(kthread_worker_fn,
+			kthread_run_perf_critical(cpu_prime_mask,
+				kthread_worker_fn,
 				&priv->disp_thread[i].worker,
 				"crtc_commit:%d", priv->disp_thread[i].crtc_id);
 		kthread_init_work(&priv->thread_priority_work,
@@ -625,7 +626,8 @@ static int msm_drm_display_thread_create(struct msm_drm_private *priv, struct dr
 		kthread_init_worker(&priv->event_thread[i].worker);
 		priv->event_thread[i].dev = ddev;
 		priv->event_thread[i].thread =
-			kthread_run(kthread_worker_fn,
+			kthread_run_perf_critical(cpu_prime_mask,
+				kthread_worker_fn,
 				&priv->event_thread[i].worker,
 				"crtc_event:%d", priv->event_thread[i].crtc_id);
 		/**
@@ -671,8 +673,8 @@ static int msm_drm_display_thread_create(struct msm_drm_private *priv, struct dr
 	 * other important events.
 	 */
 	kthread_init_worker(&priv->pp_event_worker);
-	priv->pp_event_thread = kthread_run(kthread_worker_fn,
-			&priv->pp_event_worker, "pp_event");
+	priv->pp_event_thread = kthread_run_perf_critical(cpu_prime_mask,
+			kthread_worker_fn, &priv->pp_event_worker, "pp_event");
 	kthread_init_work(&priv->thread_priority_work, msm_drm_display_thread_priority_worker);
 	kthread_queue_work(&priv->pp_event_worker, &priv->thread_priority_work);
 	kthread_flush_work(&priv->thread_priority_work);
