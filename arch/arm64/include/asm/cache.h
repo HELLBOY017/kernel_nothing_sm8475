6396da63d39ca23c9537cc6947b38dd747f1dd00
Alexander Winkowski <dereference23@outlook.com>
2021-11-27 17:25:54 +0000

    arm64: cache: Set ARCH_DMA_MINALIGN to L1_CACHE_BYTES
    Almost every arm64 SoC has 64B cache lines. There is no reason to
    waste memory only because of some exotic platforms with larger cache lines.
    
    Note that L1_CACHE_BYTES was set to 64 in the upstream commit "Revert "arm64:
    Increase the max granular size"".
    
    It was checked that the target SoC has 64B cache lines by applying the
    mentioned upstream commit and checking for warnings introduced in commit a41dc0e841523efe1df7fa5ad48b5e9027a921df.
    
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- arch/arm64/include/asm/cache.h
+++ arch/arm64/include/asm/cache.h
@@ -48,7 +48,7 @@
  * cache before the transfer is done, causing old data to be seen by
  * the CPU.
  */
-#define ARCH_DMA_MINALIGN	(128)
+#define ARCH_DMA_MINALIGN	L1_CACHE_BYTES
 
 #ifndef __ASSEMBLY__
 
