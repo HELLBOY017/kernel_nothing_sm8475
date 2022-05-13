c4ff391f5c3e5a13f030c39e17ea5bfa9074d9f0
Cosmin Tanislav <demonsingur@gmail.com>
2022-05-13 09:33:09 +0300

    techpack: display: expose forcing FOD UI for debugging
    Change-Id: I1f1b5344c9f98b541dc5c99e5d6f9c5560d8340c

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -335,6 +335,7 @@ struct dsi_panel {
 	unsigned int fod_dim_lut_len;
 	u8 fod_dim_alpha;
 	bool fod_ui;
+	bool force_fod_ui;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -476,5 +477,6 @@ int nt_display_parse_switch_cmds(struct dsi_panel *panel);
 
 bool dsi_panel_get_fod_ui(struct dsi_panel *panel);
 void dsi_panel_set_fod_ui(struct dsi_panel *panel, bool status);
+bool dsi_panel_get_force_fod_ui(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
