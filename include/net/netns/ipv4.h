7e12f23c29a5c6bb5dd9c36aa54ca27cded84ca2
Mubashir Adnan Qureshi <mubashirq@google.com>
2022-10-26 13:51:11 +0000

    tcp: add sysctls for TCP PLB parameters
    PLB (Protective Load Balancing) is a host based mechanism for load
    balancing across switch links. It leverages congestion signals(e.g. ECN)
    from transport layer to randomly change the path of the connection
    experiencing congestion. PLB changes the path of the connection by
    changing the outgoing IPv6 flow label for IPv6 connections (implemented
    in Linux by calling sk_rethink_txhash()). Because of this implementation
    mechanism, PLB can currently only work for IPv6 traffic. For more
    information, see the SIGCOMM 2022 paper:
      https://doi.org/10.1145/3544216.3544226
    
    This commit adds new sysctl knobs and sets their default values for
    TCP PLB.
    
    Signed-off-by: Mubashir Adnan Qureshi <mubashirq@google.com>
    Signed-off-by: Yuchung Cheng <ycheng@google.com>
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -185,6 +185,11 @@ struct netns_ipv4 {
 	atomic_t tfo_active_disable_times;
 	unsigned long tfo_active_disable_stamp;
 	int sysctl_tcp_reflect_tos;
+	u8 sysctl_tcp_plb_enabled;
+	u8 sysctl_tcp_plb_idle_rehash_rounds;
+	u8 sysctl_tcp_plb_rehash_rounds;
+	u8 sysctl_tcp_plb_suspend_rto_sec;
+	int sysctl_tcp_plb_cong_thresh;
 
 	int sysctl_udp_wmem_min;
 	int sysctl_udp_rmem_min;
