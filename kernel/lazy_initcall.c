4ea72ee1a61420242437c472a4c3b4a8015ae8c0
Abhay Singh Gill <abhaygill017@gmail.com>
2024-02-02 04:28:11 +0530

    lazy_initcall: Ignore qca6490 module

--- kernel/lazy_initcall.c
+++ kernel/lazy_initcall.c
@@ -52,6 +52,7 @@ static const __initconst char * const blacklist[] = {
 	"phy_qcom_ufs_qmp_v4_lahaina",
 	"phy_qcom_ufs_qmp_v4_parrot",
 	"phy_qcom_ufs_qmp_v4_waipio",
+	"qca6490",
 	"qcom_ipc_logging",
 	"qcom_logbuf_vendor_hooks",
 	"rimps_log",
