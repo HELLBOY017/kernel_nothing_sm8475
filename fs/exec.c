db35de642bce77d0e652675692363c5907fe05a8
Abhay Singh Gill <abhaygill017@gmail.com>
2024-03-20 05:58:57 +0530

    treewide: Rename KernelSU config
    After this change:-
    https://github.com/tiann/KernelSU/commit/97faab6be44132e18361fadf4ea32466b0bc50bd#diff-3a325663233178293ee38b8161f3be511a466af7e0156b9d03d5aed0497564bf
    
    KernelSU needs the KSU config in order to build, boolean is used for it in this commit as this kernel is non-GKI.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1863,7 +1863,7 @@ out_files:
 	return retval;
 }
 
-#ifdef CONFIG_KERNELSU
+#ifdef CONFIG_KSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
 #endif
@@ -1891,7 +1891,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 		goto out_ret;
 	}
 
-#ifdef CONFIG_KERNELSU
+#ifdef CONFIG_KSU
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 #endif
 
